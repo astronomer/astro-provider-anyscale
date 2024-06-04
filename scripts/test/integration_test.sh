50c08b7ab3e6027dc0231770ea348c7f1ad87c24
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-04 18:35:29 -0400

    added hatch integration

---
+++ scripts/test/integration_test.sh
@@ -0,0 +1,6 @@
+pytest -vv \
+    --cov=cosmos \
+    --cov-report=term-missing \
+    --cov-report=xml \
+    --durations=0 \
+    -m integration
---
+++ scripts/test/unit_cov.sh
@@ -0,0 +1,7 @@
+pytest \
+    -vv \
+    --cov=anyscale_provider \
+    --cov-report=term-missing \
+    --cov-report=xml \
+    --durations=0 \
+    -m "not (integration or perf)"
