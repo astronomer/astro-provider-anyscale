2d50cb31468ec6a380b41837864abae41fcedc61
Venkat <venkatajagannath.yellapragada@astronomer.io>
2024-06-07 23:16:11 -0400

    dag code updated

--- anyscale_provider/example_dags/anyscale_dag.py
+++ anyscale_provider/example_dags/anyscale_dag.py
@@ -16,7 +16,7 @@ default_args = {
 ANYSCALE_CONN_ID = "anyscale_conn"
 
 # Constants
-FOLDER_PATH = '/usr/local/airflow/dags/ray_scripts'
+FOLDER_PATH = './ray_scripts'
 
 dag = DAG(
     'sample_anyscale_workflow',
