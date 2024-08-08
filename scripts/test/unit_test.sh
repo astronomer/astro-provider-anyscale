67dbf4bdc19a98267fbd0059b353da20aec375db
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2024-08-08 15:04:13 +0530

    Fix mis code coverage job issue (#46)
    * Fix mis code coverage job issue
    
    * Update anyscale_provider/operators/anyscale.py

--- scripts/test/unit_test.sh
+++ scripts/test/unit_test.sh
@@ -1,4 +1,8 @@
 pytest \
     -vv \
+    --cov=anyscale_provider \
+    --cov-report=term-missing \
+    --cov-report=xml \
+    --durations=0 \
     --durations=0 \
     -m "not (integration or perf)"
